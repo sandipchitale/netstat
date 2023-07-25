701e38d8782576a798e61dcc0ba7e5bd2e52123d
SANDIP CHITALE <sandipchitale@gmail.com>
2023-07-24 23:02:25 -0700

    Support CLI mode and Web Application mode.

--- src/main/java/sandipchitale/netstat/NetstatApplication.java
+++ src/main/java/sandipchitale/netstat/NetstatApplication.java
@@ -3,12 +3,14 @@ package sandipchitale.netstat;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
 import org.springframework.context.annotation.Bean;
 import sandipchitale.netstat.service.NetstatService;
 
 @SpringBootApplication
 public class NetstatApplication {
 	@Bean
+	@ConditionalOnNotWebApplication
 	public CommandLineRunner commandLineRunner(NetstatService netstatLineService) {
 		return args -> {
 			netstatLineService.getNetstatLines().forEach(System.out::println);
